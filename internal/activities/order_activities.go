6bc31c6f972a08d9d9c07f4aba07a1edab52fa23
vogiaan1904 <vogiaan1904@gmail.com>
2025-05-06 15:53:09 +0700

    add proto submodule

---
+++ internal/activities/order_activities.go
@@ -0,0 +1,52 @@
+package activities
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	orderpb "github.com/vogiaan1904/order-svc/protogen/golang/order"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type OrderActivities struct {
+	OrderSvcAddr string
+}
+
+func (a *OrderActivities) GetOrder(ctx context.Context, orderID string) (*orderpb.Order, error) {
+	conn, err := grpc.Dial(a.OrderSvcAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to order service: %w", err)
+	}
+	defer conn.Close()
+
+	client := orderpb.NewOrderServiceClient(conn)
+	resp, err := client.GetOrder(ctx, &orderpb.GetOrderRequest{Id: orderID})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get order: %w", err)
+	}
+
+	log.Printf("Retrieved order %s: %v", orderID, resp.Order)
+	return resp.Order, nil
+}
+
+func (a *OrderActivities) UpdateOrderStatus(ctx context.Context, orderID string, status orderpb.OrderStatus) error {
+	conn, err := grpc.Dial(a.OrderSvcAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return fmt.Errorf("failed to connect to order service: %w", err)
+	}
+	defer conn.Close()
+
+	client := orderpb.NewOrderServiceClient(conn)
+	_, err = client.UpdateOrderStatus(ctx, &orderpb.UpdateOrderStatusRequest{
+		OrderId: orderID,
+		Status:  status,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to update order status: %w", err)
+	}
+
+	log.Printf("Updated order %s status to %s", orderID, status.String())
+	return nil
+}
